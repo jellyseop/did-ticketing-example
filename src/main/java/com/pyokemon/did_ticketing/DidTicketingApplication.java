11999f93722e3874dd87d31736da541e6cd834b4
jellyseop <jellyjun7777@naver.com>
2025-07-23 11:22:40 +0900

    init

---
+++ src/main/java/com/pyokemon/did_ticketing/DidTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.pyokemon.did_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyokemon/did_ticketing/DidTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyokemon.did_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
